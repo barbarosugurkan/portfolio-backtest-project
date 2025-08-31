ed7629de807258531f89f53e2eeddb2157bb16d4
barbarosugurkan <barbaros.ugurkan2@hotmail.com>
2025-08-31 22:41:09 +0300

    feat: Documents about financials has changed to add quarter, trailling twelve months and cumulative data seperately

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -6,10 +6,10 @@ CREATE INDEX IF NOT EXISTS idx_prices_company_date
     ON price (company_id, date);
 
 CREATE INDEX IF NOT EXISTS idx_financials_company_period
-    ON financial (company_id, period);
+    ON financial (company_id, period_year,period_month);
 
 CREATE INDEX IF NOT EXISTS idx_ratios_company_period
-    ON ratio (company_id, date);
+    ON ratio (company_id, period_year,period_month);;
     
 CREATE INDEX IF NOT EXISTS idx_multiples_company_period
     ON multiple (company_id, date_of_price);
--- src/fetch_financials.py
+++ src/fetch_financials.py
@@ -5,11 +5,11 @@ from isyatirimhisse import fetch_financials
 
 conn = sqlite3.connect("C:/Users/KULLANICI/Desktop/portfolio-backtest-project/data/database.db")
 
-ticker_dict = {"THYAO":2,"BIMAS":1}
+ticker_dict = {"THYAO":2,"BIMAS":1,"HTTBT":3}
 
 df = fetch_financials(
     symbols=list(ticker_dict.keys()),
-    start_year=2025,
+    start_year=2022,
     end_year=2025,
     exchange="TRY",
     financial_group='1',
@@ -33,8 +33,8 @@ df_pivoted = df_melted.pivot_table(
 ).reset_index()
 df_pivoted.columns.name = None
 df_pivoted.columns = df_pivoted.columns.str.lower().str.replace(" ","_")
-df_pivoted = df_pivoted.rename(columns={'net_sales': 'revenue',"gross_profit_(loss)": "gross_profit","operating_profits":"operating_profit","net_profit_after_taxes":"net_income","long_term_liabilities":"long_term_debt","short_term_liabilities":"short_term_debt","shareholders_equity":"equity"})
-df_pivoted['ebitda'] = (df_pivoted['gross_profit'] - 
+df_pivoted = df_pivoted.rename(columns={'net_sales': 'revenue_c',"gross_profit_(loss)": "gross_profit_c","operating_profits":"operating_profit_c","net_profit_after_taxes":"net_income_c","long_term_liabilities":"long_term_debt","short_term_liabilities":"short_term_debt","shareholders_equity":"equity","taxation_on_continuing_operations":"taxation_on_continuing_operations_c","profit_before_tax_from_continuing_operations":"profit_before_tax_from_continuing_operations_c","diluted_earnings_per_share":"eps_c"})
+df_pivoted['ebitda_c'] = (df_pivoted['gross_profit_c'] - 
                df_pivoted['marketing_selling_&_distrib._expenses_(-)'].abs() -
                df_pivoted['general_administrative_expenses_(-)'].abs() - 
                df_pivoted['research_&_development_expenses_(-)'].abs() +
@@ -50,8 +50,29 @@ df_pivoted['gross_debt'] = (
 df_pivoted['net_debt'] = df_pivoted['gross_debt'] - df_pivoted["cash\xa0and\xa0cash\xa0equivalents"] - df_pivoted['short-term_financial_investments']
 df_pivoted['company_id'] = df_pivoted['symbol'].map(ticker_dict)
 df_pivoted["date_of_publish"] = np.nan
+df_pivoted[["period_year","period_month"]] = df_pivoted["period"].str.split("/",expand=True)
+df_pivoted['period_month'] = pd.to_numeric(df_pivoted['period_month'])
+df_pivoted['period_year'] = pd.to_numeric(df_pivoted['period_year'])
 
-df_pivoted = df_pivoted[['company_id',"period","date_of_publish","revenue","gross_profit","operating_profit","ebitda","net_income","current_assets","fixed_assets","long_term_debt","short_term_debt","gross_debt","net_debt","equity"]]
+df_pivoted = df_pivoted.sort_values(by=['company_id', 'period_year', 'period_month']).reset_index(drop=True)
+kalemler = ["revenue","gross_profit","operating_profit","ebitda","net_income","taxation_on_continuing_operations","profit_before_tax_from_continuing_operations","eps"]
+
+for kalem in kalemler:
+    kalem_q = kalem + "_q"
+    kalem_c = kalem + "_c"
+    kalem_ttm = kalem + "_ttm"
+
+    df_pivoted[kalem_q] = np.where(
+        df_pivoted['period_month'] == 3,
+        df_pivoted[kalem_c],
+        df_pivoted.groupby(['company_id',"period_year"])[kalem_c].diff()
+    )
+
+    df_pivoted[kalem_ttm] = df_pivoted.groupby('company_id')[kalem_q].rolling(window=4).sum().values
+
+df_pivoted["effective_tax_rate_ttm"] = df_pivoted["taxation_on_continuing_operations_ttm"] + df_pivoted["profit_before_tax_from_continuing_operations_ttm"]
+
+df_pivoted = df_pivoted[['company_id',"period_year","period_month","date_of_publish","revenue_ttm","gross_profit_ttm","operating_profit_ttm","ebitda_ttm","net_income_ttm","revenue_q","gross_profit_q","operating_profit_q","ebitda_q","net_income_q","revenue_c","gross_profit_c","operating_profit_c","ebitda_c","net_income_c","effective_tax_rate_ttm","current_assets","fixed_assets","long_term_debt","short_term_debt","gross_debt","net_debt","equity","eps_c","eps_q","eps_ttm"]]
 
 df_pivoted.to_sql('financial', conn, if_exists='append', index=False)
 conn.close()
