1a4f54a97bcf975c7550903f2515f5808a041763
barbarosugurkan <barbaros.ugurkan2@hotmail.com>
2025-09-04 23:45:48 +0300

    feat: Pytests are added, problems about attempts for adding existing data to database is solved.

--- src/fetch_prices.py
+++ src/fetch_prices.py
@@ -1,47 +1,156 @@
 import sqlite3
 import pandas as pd
 import yfinance as yf
+import json
+import os
 from isyatirimhisse import fetch_stock_data
 from datetime import datetime
 
-conn = sqlite3.connect("C:/Users/KULLANICI/Desktop/portfolio-backtest-project/data/database.db")
+# yf verilerini çekip işleyen fonksiyon
+def _fetch_and_process_yf(ticker_dict: dict, start_date: str, end_date: str):
 
-ticker_dict = ticker_dict = {"BIMAS.IS": 1, "THYAO.IS": 2, "HTTBT.IS": 3}
-tickers = list(ticker_dict.keys())
-start_date = '2022-01-01'
-end_date = '2025-12-31'
+    tickers = list(ticker_dict.keys())
 
-start_date_object = datetime.strptime(start_date, "%Y-%m-%d")
-end_date_object = datetime.strptime(end_date, "%Y-%m-%d")
+    try:
+        data = yf.download(tickers, start=start_date, end=end_date)
+    except Exception as e: 
+        print(f"Hata: Veri çekilemedi. Bağlantı sorunu olabilir. Hata detayları: {e}")
+        return pd.DataFrame()
+    
+    if data.empty:
+        return pd.DataFrame()
 
-data = yf.download(tickers, start=start_date, end=end_date)
-data.columns = ['_'.join(col).strip() for col in data.columns.values]
-data = data.reset_index()
-data = pd.melt(data, id_vars=['Date'], var_name='ticker_info', value_name='value')
-data[['data_type', 'ticker']] = data['ticker_info'].str.split('_', expand=True)
-data.drop('ticker_info', axis=1, inplace=True)
+    data.columns = ['_'.join(col).strip() for col in data.columns.values]
+    data = data.reset_index()
+    data = pd.melt(data, id_vars=['Date'], var_name='ticker_info', value_name='value')
+    data[['data_type', 'ticker']] = data['ticker_info'].str.split('_', expand=True)
+    data.drop('ticker_info', axis=1, inplace=True)
 
-data = data.pivot_table(index=['Date', 'ticker'], columns='data_type', values='value').reset_index()
-data.columns.name = None
-data.columns = data.columns.str.lower()
-data['company_id'] = data['ticker'].map(ticker_dict)
+    data = data.pivot_table(index=['Date', 'ticker'], columns='data_type', values='value').reset_index()
+    data.columns.name = None
+    data.columns = data.columns.str.lower()
+    data['company_id'] = data['ticker'].map(ticker_dict)
 
-mc_df = fetch_stock_data(
-    symbols=[s[:-3] for s in tickers],
-    start_date=start_date_object.strftime("%d-%m-%Y"),
-    end_date=end_date_object.strftime("%d-%m-%Y"),
-    save_to_excel=False
-)
-mc_df = mc_df[["HGDG_HS_KODU","HGDG_TARIH","PD"]]
-mc_df = mc_df.rename(columns={"HGDG_HS_KODU": "ticker", "HGDG_TARIH": "date","PD":"market_cap"})
-mc_df["ticker"] = mc_df["ticker"] + ".IS"
-data = data.merge(
-    mc_df,
-    how="left",
-    left_on=["ticker", "date"],
-    right_on=["ticker", "date"]
-)
-data.drop('ticker', axis=1, inplace=True)
+    data = data.drop_duplicates(subset=["date", "ticker"], keep="last") # eğer aynı veriden iki tane varsa en sonuncusu kalsın
 
-data.to_sql('price', conn, if_exists='append', index=False)
-conn.close()
+    return data
+
+# isyatirimhisse'den gelen market_cap verisini ekleyen fonksiyon
+def _fetch_and_merge_is(yf_df: pd.DataFrame,ticker_dict: dict, start_date: str, end_date: str):
+
+    tickers = list(ticker_dict.keys())
+
+    start_date_object = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date_object = datetime.strptime(end_date, "%Y-%m-%d")
+
+    mc_df = fetch_stock_data(
+        symbols=[s[:-3] for s in tickers],
+        start_date=start_date_object.strftime("%d-%m-%Y"),
+        end_date=end_date_object.strftime("%d-%m-%Y"),
+        save_to_excel=False
+    )
+
+    expected_cols = ["HGDG_HS_KODU","HGDG_TARIH","PD"]
+    if not mc_df.empty and all(col in mc_df.columns for col in expected_cols):
+        mc_df = mc_df[expected_cols]
+        mc_df = mc_df.rename(columns={"HGDG_HS_KODU": "ticker", "HGDG_TARIH": "date","PD":"market_cap"})
+        mc_df["ticker"] = mc_df["ticker"] + ".IS"
+        mc_df = mc_df.drop_duplicates(subset=["date", "ticker"], keep="last")
+
+        mc_df['date'] = pd.to_datetime(mc_df['date'])
+
+        merged_df = yf_df.merge(
+            mc_df,
+            how="left",
+            left_on=["ticker", "date"],
+            right_on=["ticker", "date"]
+        )
+    else:
+        merged_df = yf_df
+        merged_df["market_cap"] = pd.NA
+
+    
+    merged_df.drop('ticker', axis=1, inplace=True)
+
+    return merged_df
+    
+
+def _get_ticker_dict() -> dict:
+    """Proje kök dizinindeki config klasöründen ticker sözlüğünü okur."""
+    
+    # Mevcut dosyanın (fetch_prices.py) dizinini bulur
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Bir seviye yukarı çıkarak proje kök dizinine ulaşır
+    project_root = os.path.dirname(current_dir)
+    
+    # JSON dosyasının tam yolunu oluşturur
+    json_path = os.path.join(project_root, 'config', "ticker_dict.json")
+    
+    # Dosyayı açar ve veriyi okur
+    try:
+        with open(json_path, 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print(f"Hata: Konfigürasyon dosyası bulunamadı. Lütfen '{json_path}' adresini kontrol edin.")
+        return {}
+    
+
+def fetch_prices(conn, ticker_dict: dict, start_date: str, end_date: str):
+    yfinance_df = _fetch_and_process_yf(ticker_dict, start_date, end_date)
+    if yfinance_df.empty:
+        print("yfinance'ten veri alınamadı. İşlem durduruldu.")
+        return pd.DataFrame()
+
+    final_df = _fetch_and_merge_is(yfinance_df, ticker_dict, start_date, end_date)
+
+    # -----------------------------------------------------------
+    # Bu koyacağımız veri database'de zaten var mı ona bakacağız.
+    # -----------------------------------------------------------
+    if conn is not None:
+        try:
+            # Sadece ilgili tarih aralığını DB’den çek
+            query = """
+            SELECT company_id, date 
+            FROM price 
+            WHERE date BETWEEN ? AND ?
+            """
+            existing_data = pd.read_sql_query(query, conn, params=[start_date, end_date])
+            existing_data['date'] = pd.to_datetime(existing_data['date'])
+
+            merged_df = final_df.merge(
+                existing_data,
+                on=['company_id', 'date'],
+                how='left',
+                indicator=True
+            )
+
+            # Uyarı mesajı
+            dups = merged_df[merged_df['_merge'] == 'both']
+            if not dups.empty:
+                for _, row in dups.iterrows():
+                    print(f"Uyarı: company_id {row['company_id']} için {row['date'].strftime('%Y-%m-%d')} verisi zaten bulunuyor.")
+
+            final_df = merged_df[merged_df['_merge'] == 'left_only'].drop(columns=['_merge'])
+
+        except Exception as e:
+            print(f"Hata: Veritabanından mevcut veri çekilemedi. İşlem durduruldu: {e}")
+            return pd.DataFrame()
+
+    return final_df.drop_duplicates(subset=["date", "company_id"], keep="last")
+
+
+
+if __name__ == "__main__":
+    db_path = "C:/Users/KULLANICI/Desktop/portfolio-backtest-project/data/database.db"
+
+    ticker_dict = _get_ticker_dict()
+    start_date = '2025-08-22'
+    end_date = '2025-09-03'
+
+    conn = sqlite3.connect(db_path)
+    final_df = fetch_prices(conn,ticker_dict,start_date,end_date)
+
+    final_df.to_sql('price', conn, if_exists='append', index=False)
+    conn.close()
+    print("Price fetch completed.")
